99aff4465393de025829a89711ae315b1f332b44
Harold Labruyere <hlabruyere@hotmail.com>
2023-04-03 16:24:26 +0200

    Initial Commit

---
+++ Advent2021/aocutils.py
@@ -0,0 +1,138 @@
+import os
+import re
+import requests
+from typing import Optional, Any
+from constants import (
+  AOC_URL,
+  DAY_PREFIX,
+  END_MARK,
+  INPUTS_ROOT,
+  MARKS,
+  SOLUTIONS_ROOT,
+  USER_AGENT,
+  USER_SESSION_ID_FILE_PATH,
+  YEAR_PREFIX
+)
+
+def pad_day(day: int) -> str:
+  '''
+  Pad a day value with zeros so that it sorts correctly with `ls`.
+  '''
+  return str(day).zfill(2)
+
+def get_day_id(day: int) -> str:
+  '''
+  Gets the day identifier for a given day number.
+  >>> get_day_id(5)
+  'd05'
+  '''
+  return f'{DAY_PREFIX}{pad_day(day)}'
+
+def get_year_id(year: int) -> str:
+  '''
+  Gets the year identifier for a given year number.
+  >>> get_year_id(2018)
+  'y2018'
+  '''
+  return f'{YEAR_PREFIX}{year}'
+
+def highlight(text: Any, color: str='b') -> str:
+  '''
+  Surround a value in ANSI escape codes to make it stand out when printed.
+  '''
+  return f'{MARKS[color]}{text}{END_MARK}'
+
+def get_latest_year() -> Optional[int]:
+  '''
+  Search the solutions directory for the latest year, and return it.
+  Returns None if directory is empty.
+  '''
+  try:
+    return max(
+      int(entry.split(YEAR_PREFIX)[1])
+      for entry in os.listdir(SOLUTIONS_ROOT)
+      if re.fullmatch(r'y\d+', entry)
+    )
+  except ValueError:
+    return None
+
+def get_user_session_id() -> str:
+  '''
+  Retrieves unique user session id from the file in the project, or asks the user to provide it if the file doesn't exist.
+  '''
+  if os.path.isfile(USER_SESSION_ID_FILE_PATH):
+    with open(USER_SESSION_ID_FILE_PATH) as f:
+      return f.read().strip()
+  else:
+    done = False
+    user_session_id = ''
+    print('What is your session id? It\'s needed for downloading puzzle inputs.')
+    print('Your id is the value of the cookie named', highlight('session'), f'on {highlight(AOC_URL)}.')
+    while not done:
+      user_session_id = input(f"{highlight('>', color='r')} ")
+      user_session_id = user_session_id.strip()
+      if re.fullmatch(r'[\da-f]+', user_session_id):
+        with open(USER_SESSION_ID_FILE_PATH, 'w') as f:
+          f.write(user_session_id)
+        done = True
+        print('Thanks.')
+      else:
+        print('That\'s not a valid id. It should consist only of digits and lowercase letters a-f. Please try again.')
+    return user_session_id
+
+def download_input(year: int, day: int, input_file_path: str) -> bool:
+  '''
+  Attempts to download and save the given input from the Advent of Code site.
+  Returns a boolean indicating success/failure.
+  '''
+  print('Input file', highlight(input_file_path, color='r'), 'does not exist.')
+  print(f'Attempting to download puzzle input from {highlight(AOC_URL)}.')
+  result = None
+  error_count = 0
+  while result is None:
+    try:
+      response = requests.get(
+        url=f'{AOC_URL}/{year}/day/{day}/input',
+        cookies={'session': get_user_session_id()},
+        headers={'User-Agent': USER_AGENT}
+      )
+      if response.ok:
+        os.makedirs(os.path.dirname(input_file_path), exist_ok=True)
+        with open(input_file_path, 'w') as f:
+          f.write(response.text)
+        print(highlight('Success.', color='g'), 'Input downloaded and saved to', f'{highlight(input_file_path)}.')
+        result = True
+      else:
+        print('Server responded with a non-200 status code:', f"{highlight(response.status_code, color='y')}.")
+        print('Aborting.')
+        result = False
+    except requests.exceptions.RequestException:
+      error_count += 1
+      if error_count >= 2:
+        print('Giving up.')
+        result = False
+      elif error_count == 0:
+        print('Error while requesting input from server. Request probably timed out. Trying again.')
+      else:
+        print('Trying again.')
+    except Exception as e:
+      print('Unhandled error while requesting input from server. ' + str(e))
+      result = False
+  print()
+  return result
+
+
+def get_input(year: int, day: int) -> str:
+  '''
+  Loads an input file into a string and returns it.
+  '''
+  input_file_path = os.path.join(
+    INPUTS_ROOT,
+    str(year),
+    pad_day(day)
+  )
+  if not os.path.isfile(input_file_path) and not download_input(year, day, input_file_path):
+    raise Exception('Could not retrieve input for the puzzle.')
+  with open(input_file_path, 'r') as f:
+    # strip newline at end of file
+    return f.read()[:-1]
---
+++ Advent2021/constants.py
@@ -0,0 +1,54 @@
+from collections import defaultdict
+import os
+
+# for downloading puzzle inputs
+AOC_URL = 'https://adventofcode.com'
+USER_AGENT = 'advent_of_code_python_input_downloader_jzimbel'
+
+# base directories and other file names
+SOLUTIONS_DIR_NAME = 'solutions'
+PROJECT_ROOT = os.path.dirname(__file__)
+TESTS_ROOT = os.path.join(PROJECT_ROOT, 'tests')
+SOLUTIONS_ROOT = os.path.join(PROJECT_ROOT, SOLUTIONS_DIR_NAME)
+INPUTS_ROOT = os.path.join(PROJECT_ROOT, 'inputs')
+USER_SESSION_ID_FILE_PATH = os.path.join(PROJECT_ROOT, '.USER_SESSION_ID')
+
+# command names
+COMMAND_RUN_SOLUTION = 'run_solution'
+COMMAND_MAKE_NEW_YEAR = 'make_new_year'
+
+# constants used when initializing a new year directory and loading inputs/solutions
+YEAR_PREFIX = 'y'
+DAY_PREFIX = 'd'
+
+# starter file templates
+SOLUTION_FILE_TEMPLATE = \
+f"""'''
+Solution for day {{day}} of the {{year}} Advent of Code calendar.
+Run it with the command `python -m adventofcode {COMMAND_RUN_SOLUTION} -y {{year}} {{day}}` from the project root.
+'''
+from adventofcode.types import Solution
+def run(data: str) -> Solution:
+  # not yet implemented!
+  return (None, None)
+"""
+
+TEST_FILE_TEMPLATE = \
+f"""'''
+Test for year {{year}}, day {{day}} solution.
+Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
+'''
+from adventofcode.{SOLUTIONS_DIR_NAME}.{YEAR_PREFIX}{{year}}.{DAY_PREFIX}{{zero_padded_day}} import run
+def test_run() -> None:
+  # not yet implemented!
+  assert run('') == (None, None)
+"""
+
+# for highlighting sections of printed text
+MARKS = defaultdict(lambda: '\033[1;34m', {
+  'b': '\033[1;34m',
+  'g': '\033[1;32m',
+  'y': '\033[1;33m',
+  'r': '\033[1;31m'
+})
+END_MARK = '\033[0m'
